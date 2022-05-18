d860ff5995ce0e25228b7ceac82f3f8635a72d46
Sayrex <abdullohjalilov8@gmail.com>
2022-05-18 22:04:51 +0500

    end

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Hello world");
+console.log(18);
+console.log(true);
+console.log(false);
+console.log(null);
+console.log(undefined);
+
+var username = "Abdulloh"
+
+console.log(username);
+
+var age = 16
+
+console.log(age);
+
+var text = "Abdulloh" + "16"
+
+console.log(text);
+
+var item = "Abdulloh" + "Pubg"
+
+console.log(username + "Pubg");
+
+console.log(10 + 8);
+console.log(10 - 8);
+console.log(10 * 8);
+console.log(10 / 8);
+
+var a = prompt("UY ISHI TOG'RIMI?")
+
+console.log(a);
+
+alert(a)
